8773de221e062bb98b4c292ba3c85940ce5e27a8
Salma Ali <salma_fouad@outlook.com>
2024-03-17 15:10:24 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: 'http://localhost:3000',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   });
+//   await app.listen(3001);
+// }
+// bootstrap();
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  };
+  app.enableCors(corsOptions);
+  await app.listen(3001);
+}
+bootstrap();
